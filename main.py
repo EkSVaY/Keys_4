0756b42cc385a82da783b37db2b86dbdacb01a5e
IlyaSokol17 <159280837+IlyaSokol17@users.noreply.github.com>
2024-03-19 19:38:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,24 @@
 #
 import ru_local as ru
 
+def det_protection(protectionism):
+    '''
+    The function returns true or false, depending on whether the country is pursuing
+    a protectionist policyThe function returns true or false, depending on whether the country
+    is pursuing a protectionist policy
+    '''
+    return protectionism == "no"
+
+
+def medium_candy_shops(people, candy_shops):
+    '''
+    The function returns true if the number of factories per 100,000 people
+    does not exceed the number of factories
+    already built, and False on the contrary.
+    '''
+    if people % 100000 == 0:
+        return candy_shops < (people // 100000)
+    return candy_shops < (people // 100000) + 1
 
 def main():
     countries = []
