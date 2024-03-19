105bf29b1644f7fd939ce35084d984a974de55b5
EkSVaY <v.ekin@g.nsu.ru>
2024-03-19 19:32:45 +0700

    1

---
+++ main.py
@@ -0,0 +1,26 @@
+# Case-study #3
+# Developers: Ekin Viacheslaw, Amat Tolkochokov, Ilya Sokolov, Askar Gazizov
+#
+import ru_local as ru
+
+
+def main():
+    countries = []
+    protectionism = []
+    population = []
+    factories = []
+    love_candy = []
+    amount = 0
+
+    while True:
+        countries.append(input(ru.COUNTRY_NAME))
+        protectionism.append(input(ru.PROTECTIONISM).lower())
+        population.append(int(input(ru.PEOPLE)))
+        factories.append(int(input(ru.FABRICS)))
+        love_candy.append(int(input(ru.LOVE)))
+        vote = input(ru.CHOOSE).lower()
+        print()
+        if vote == 'yes':
+            pass
+        elif vote == 'no':
+            break
